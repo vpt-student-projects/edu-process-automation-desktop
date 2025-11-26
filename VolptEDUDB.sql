529f5635b1560fade2908163d9cb171fa6d63150
KyaroMeow <164043414+KyaroMeow@users.noreply.github.com>
2025-11-26 23:02:09 +0300

    fix db
    add autoincrement

--- VolptEDUDB.sql
+++ VolptEDUDB.sql
@@ -2,12 +2,12 @@
 -- PostgreSQL database dump
 --
 
-\restrict hc1cDaKgddJ2aXbQX9dOlMieOvp5VTWhOpcUSw4TYheCRH4q86SUiAQmthPBfaS
+\restrict w7Cja76yk1I45SJ8AsPvqDgjzaxQpy4i10ptMchrujBdKLaZ8EFsc1nt2Jfen4y
 
 -- Dumped from database version 18.0
 -- Dumped by pg_dump version 18.0
 
--- Started on 2025-11-16 21:21:28
+-- Started on 2025-11-26 22:56:23
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -32,7 +32,7 @@ CREATE SCHEMA public;
 ALTER SCHEMA public OWNER TO pg_database_owner;
 
 --
--- TOC entry 5001 (class 0 OID 0)
+-- TOC entry 5019 (class 0 OID 0)
 -- Dependencies: 4
 -- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: pg_database_owner
 --
@@ -73,6 +73,21 @@ CREATE TABLE public."AttendanceType" (
 ALTER TABLE public."AttendanceType" OWNER TO postgres;
 
 --
+-- TOC entry 234 (class 1259 OID 16587)
+-- Name: Attendance_Id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public."Attendance" ALTER COLUMN "Id" ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public."Attendance_Id_seq"
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
 -- TOC entry 225 (class 1259 OID 16446)
 -- Name: Grade; Type: TABLE; Schema: public; Owner: postgres
 --
@@ -88,6 +103,21 @@ CREATE TABLE public."Grade" (
 ALTER TABLE public."Grade" OWNER TO postgres;
 
 --
+-- TOC entry 236 (class 1259 OID 16589)
+-- Name: Grade_Id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public."Grade" ALTER COLUMN "Id" ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public."Grade_Id_seq"
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
 -- TOC entry 220 (class 1259 OID 16394)
 -- Name: Group; Type: TABLE; Schema: public; Owner: postgres
 --
@@ -99,6 +129,21 @@ CREATE TABLE public."Group" (
 
 
 ALTER TABLE public."Group" OWNER TO postgres;
+
+--
+-- TOC entry 233 (class 1259 OID 16586)
+-- Name: Group_Id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public."Group" ALTER COLUMN "Id" ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public."Group_Id_seq"
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
 
 --
 -- TOC entry 223 (class 1259 OID 16424)
@@ -119,6 +164,21 @@ CREATE TABLE public."Lesson" (
 
 
 ALTER TABLE public."Lesson" OWNER TO postgres;
+
+--
+-- TOC entry 231 (class 1259 OID 16584)
+-- Name: Lesson_Id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public."Lesson" ALTER COLUMN "Id" ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public."Lesson_Id_seq"
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
 
 --
 -- TOC entry 219 (class 1259 OID 16387)
@@ -163,6 +223,36 @@ CREATE TABLE public."StudentTopic" (
 ALTER TABLE public."StudentTopic" OWNER TO postgres;
 
 --
+-- TOC entry 237 (class 1259 OID 16590)
+-- Name: StudentTopic_Id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public."StudentTopic" ALTER COLUMN "Id" ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public."StudentTopic_Id_seq"
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- TOC entry 235 (class 1259 OID 16588)
+-- Name: Student_Id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public."Student" ALTER COLUMN "Id" ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public."Student_Id_seq"
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
 -- TOC entry 221 (class 1259 OID 16403)
 -- Name: Subject; Type: TABLE; Schema: public; Owner: postgres
 --
@@ -175,6 +265,21 @@ CREATE TABLE public."Subject" (
 
 
 ALTER TABLE public."Subject" OWNER TO postgres;
+
+--
+-- TOC entry 238 (class 1259 OID 16591)
+-- Name: Subject_Id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public."Subject" ALTER COLUMN "Id" ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public."Subject_Id_seq"
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
 
 --
 -- TOC entry 227 (class 1259 OID 16510)
@@ -206,6 +311,21 @@ CREATE TABLE public."TopicType" (
 ALTER TABLE public."TopicType" OWNER TO postgres;
 
 --
+-- TOC entry 239 (class 1259 OID 16592)
+-- Name: Topic_Id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public."Topic" ALTER COLUMN "Id" ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public."Topic_Id_seq"
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
 -- TOC entry 222 (class 1259 OID 16412)
 -- Name: User; Type: TABLE; Schema: public; Owner: postgres
 --
@@ -213,7 +333,7 @@ ALTER TABLE public."TopicType" OWNER TO postgres;
 CREATE TABLE public."User" (
     "Id" integer NOT NULL,
     "Login" character varying NOT NULL,
-    "PasswordHash" character varying NOT NULL,
+    "PasswordHash" character varying,
     "FullName" character varying NOT NULL,
     "RoleId" integer NOT NULL
 );
@@ -222,7 +342,22 @@ CREATE TABLE public."User" (
 ALTER TABLE public."User" OWNER TO postgres;
 
 --
--- TOC entry 4991 (class 0 OID 16455)
+-- TOC entry 232 (class 1259 OID 16585)
+-- Name: User_Id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public."User" ALTER COLUMN "Id" ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public."User_Id_seq"
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- TOC entry 5000 (class 0 OID 16455)
 -- Dependencies: 226
 -- Data for Name: Attendance; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -230,15 +365,20 @@ ALTER TABLE public."User" OWNER TO postgres;
 
 
 --
--- TOC entry 4994 (class 0 OID 16534)
+-- TOC entry 5003 (class 0 OID 16534)
 -- Dependencies: 229
 -- Data for Name: AttendanceType; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
+INSERT INTO public."AttendanceType" ("Id", "Name") VALUES (5, 'от');
+INSERT INTO public."AttendanceType" ("Id", "Name") VALUES (4, 'ув');
+INSERT INTO public."AttendanceType" ("Id", "Name") VALUES (3, 'уш');
+INSERT INTO public."AttendanceType" ("Id", "Name") VALUES (2, 'оп');
+INSERT INTO public."AttendanceType" ("Id", "Name") VALUES (1, 'нб');
 
 
 --
--- TOC entry 4990 (class 0 OID 16446)
+-- TOC entry 4999 (class 0 OID 16446)
 -- Dependencies: 225
 -- Data for Name: Grade; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -246,33 +386,37 @@ ALTER TABLE public."User" OWNER TO postgres;
 
 
 --
--- TOC entry 4985 (class 0 OID 16394)
+-- TOC entry 4994 (class 0 OID 16394)
 -- Dependencies: 220
 -- Data for Name: Group; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
+INSERT INTO public."Group" ("Id", "Name") OVERRIDING SYSTEM VALUE VALUES (1, '4-22 ИСП-8');
 
 
 --
--- TOC entry 4988 (class 0 OID 16424)
+-- TOC entry 4997 (class 0 OID 16424)
 -- Dependencies: 223
 -- Data for Name: Lesson; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
+INSERT INTO public."Lesson" ("Id", "Date", "Number", "UserId", "SubjectId", "GroupId", "TopicId", "Classroom", "Comment") OVERRIDING SYSTEM VALUE VALUES (2, '2025-09-25', 4, 2, 1, 1, NULL, '216', NULL);
+INSERT INTO public."Lesson" ("Id", "Date", "Number", "UserId", "SubjectId", "GroupId", "TopicId", "Classroom", "Comment") OVERRIDING SYSTEM VALUE VALUES (3, '2025-09-26', 2, 2, 1, 1, NULL, '216', NULL);
+INSERT INTO public."Lesson" ("Id", "Date", "Number", "UserId", "SubjectId", "GroupId", "TopicId", "Classroom", "Comment") OVERRIDING SYSTEM VALUE VALUES (4, '2025-09-26', 3, 2, 1, 1, NULL, '216', NULL);
 
 
 --
--- TOC entry 4984 (class 0 OID 16387)
+-- TOC entry 4993 (class 0 OID 16387)
 -- Dependencies: 219
 -- Data for Name: Role; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public."Role" VALUES (2, 'teacher');
-INSERT INTO public."Role" VALUES (1, 'admin');
+INSERT INTO public."Role" ("Id", "Name") VALUES (2, 'teacher');
+INSERT INTO public."Role" ("Id", "Name") VALUES (1, 'admin');
 
 
 --
--- TOC entry 4989 (class 0 OID 16436)
+-- TOC entry 4998 (class 0 OID 16436)
 -- Dependencies: 224
 -- Data for Name: Student; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -280,7 +424,7 @@ INSERT INTO public."Role" VALUES (1, 'admin');
 
 
 --
--- TOC entry 4995 (class 0 OID 16566)
+-- TOC entry 5004 (class 0 OID 16566)
 -- Dependencies: 230
 -- Data for Name: StudentTopic; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -288,15 +432,16 @@ INSERT INTO public."Role" VALUES (1, 'admin');
 
 
 --
--- TOC entry 4986 (class 0 OID 16403)
+-- TOC entry 4995 (class 0 OID 16403)
 -- Dependencies: 221
 -- Data for Name: Subject; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
+INSERT INTO public."Subject" ("Id", "Name", "Total_hours") OVERRIDING SYSTEM VALUE VALUES (1, 'МДК 04.01 Внедрение и поддержка компьютерных систем', 68);
 
 
 --
--- TOC entry 4992 (class 0 OID 16510)
+-- TOC entry 5001 (class 0 OID 16510)
 -- Dependencies: 227
 -- Data for Name: Topic; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -304,23 +449,109 @@ INSERT INTO public."Role" VALUES (1, 'admin');
 
 
 --
--- TOC entry 4993 (class 0 OID 16525)
+-- TOC entry 5002 (class 0 OID 16525)
 -- Dependencies: 228
 -- Data for Name: TopicType; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
+INSERT INTO public."TopicType" ("Id", "Name") VALUES (3, 'ПЗ');
+INSERT INTO public."TopicType" ("Id", "Name") VALUES (2, 'ЛР');
+INSERT INTO public."TopicType" ("Id", "Name") VALUES (1, 'ЛЕК');
 
 
 --
--- TOC entry 4987 (class 0 OID 16412)
+-- TOC entry 4996 (class 0 OID 16412)
 -- Dependencies: 222
 -- Data for Name: User; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
+INSERT INTO public."User" ("Id", "Login", "PasswordHash", "FullName", "RoleId") OVERRIDING SYSTEM VALUE VALUES (1, 'Admin', NULL, 'admin', 1);
+INSERT INTO public."User" ("Id", "Login", "PasswordHash", "FullName", "RoleId") OVERRIDING SYSTEM VALUE VALUES (2, 'Alexey3000@gmail.com', NULL, 'Дмитриев Алексей Андреевич', 2);
 
 
 --
--- TOC entry 4819 (class 2606 OID 16542)
+-- TOC entry 5020 (class 0 OID 0)
+-- Dependencies: 234
+-- Name: Attendance_Id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public."Attendance_Id_seq"', 1, false);
+
+
+--
+-- TOC entry 5021 (class 0 OID 0)
+-- Dependencies: 236
+-- Name: Grade_Id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public."Grade_Id_seq"', 1, false);
+
+
+--
+-- TOC entry 5022 (class 0 OID 0)
+-- Dependencies: 233
+-- Name: Group_Id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public."Group_Id_seq"', 1, true);
+
+
+--
+-- TOC entry 5023 (class 0 OID 0)
+-- Dependencies: 231
+-- Name: Lesson_Id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public."Lesson_Id_seq"', 4, true);
+
+
+--
+-- TOC entry 5024 (class 0 OID 0)
+-- Dependencies: 237
+-- Name: StudentTopic_Id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public."StudentTopic_Id_seq"', 1, false);
+
+
+--
+-- TOC entry 5025 (class 0 OID 0)
+-- Dependencies: 235
+-- Name: Student_Id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public."Student_Id_seq"', 1, false);
+
+
+--
+-- TOC entry 5026 (class 0 OID 0)
+-- Dependencies: 238
+-- Name: Subject_Id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public."Subject_Id_seq"', 1, false);
+
+
+--
+-- TOC entry 5027 (class 0 OID 0)
+-- Dependencies: 239
+-- Name: Topic_Id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public."Topic_Id_seq"', 1, false);
+
+
+--
+-- TOC entry 5028 (class 0 OID 0)
+-- Dependencies: 232
+-- Name: User_Id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public."User_Id_seq"', 2, true);
+
+
+--
+-- TOC entry 4828 (class 2606 OID 16542)
 -- Name: AttendanceType AttendanceType_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -329,7 +560,7 @@ ALTER TABLE ONLY public."AttendanceType"
 
 
 --
--- TOC entry 4813 (class 2606 OID 16464)
+-- TOC entry 4822 (class 2606 OID 16464)
 -- Name: Attendance Attendance_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -338,7 +569,7 @@ ALTER TABLE ONLY public."Attendance"
 
 
 --
--- TOC entry 4811 (class 2606 OID 16454)
+-- TOC entry 4820 (class 2606 OID 16454)
 -- Name: Grade Grade_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -347,7 +578,7 @@ ALTER TABLE ONLY public."Grade"
 
 
 --
--- TOC entry 4801 (class 2606 OID 16402)
+-- TOC entry 4810 (class 2606 OID 16402)
 -- Name: Group Group_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -356,7 +587,7 @@ ALTER TABLE ONLY public."Group"
 
 
 --
--- TOC entry 4807 (class 2606 OID 16435)
+-- TOC entry 4816 (class 2606 OID 16435)
 -- Name: Lesson Lesson_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -365,7 +596,7 @@ ALTER TABLE ONLY public."Lesson"
 
 
 --
--- TOC entry 4799 (class 2606 OID 16393)
+-- TOC entry 4808 (class 2606 OID 16393)
 -- Name: Role Role_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -374,7 +605,7 @@ ALTER TABLE ONLY public."Role"
 
 
 --
--- TOC entry 4821 (class 2606 OID 16573)
+-- TOC entry 4830 (class 2606 OID 16573)
 -- Name: StudentTopic StudentTopic_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -383,7 +614,7 @@ ALTER TABLE ONLY public."StudentTopic"
 
 
 --
--- TOC entry 4809 (class 2606 OID 16445)
+-- TOC entry 4818 (class 2606 OID 16445)
 -- Name: Student Student_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -392,7 +623,7 @@ ALTER TABLE ONLY public."Student"
 
 
 --
--- TOC entry 4803 (class 2606 OID 16411)
+-- TOC entry 4812 (class 2606 OID 16411)
 -- Name: Subject Subject_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -401,7 +632,7 @@ ALTER TABLE ONLY public."Subject"
 
 
 --
--- TOC entry 4817 (class 2606 OID 16533)
+-- TOC entry 4826 (class 2606 OID 16533)
 -- Name: TopicType TopicType_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -410,7 +641,7 @@ ALTER TABLE ONLY public."TopicType"
 
 
 --
--- TOC entry 4815 (class 2606 OID 16515)
+-- TOC entry 4824 (class 2606 OID 16515)
 -- Name: Topic Topic_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -419,7 +650,7 @@ ALTER TABLE ONLY public."Topic"
 
 
 --
--- TOC entry 4805 (class 2606 OID 16423)
+-- TOC entry 4814 (class 2606 OID 16423)
 -- Name: User User_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -428,7 +659,7 @@ ALTER TABLE ONLY public."User"
 
 
 --
--- TOC entry 4830 (class 2606 OID 16500)
+-- TOC entry 4839 (class 2606 OID 16500)
 -- Name: Attendance FK_Attendance_LessonId; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -437,7 +668,7 @@ ALTER TABLE ONLY public."Attendance"
 
 
 --
--- TOC entry 4831 (class 2606 OID 16505)
+-- TOC entry 4840 (class 2606 OID 16505)
 -- Name: Attendance FK_Attendance_StudentId; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -446,7 +677,7 @@ ALTER TABLE ONLY public."Attendance"
 
 
 --
--- TOC entry 4832 (class 2606 OID 16561)
+-- TOC entry 4841 (class 2606 OID 16561)
 -- Name: Attendance FK_Attendance_TypeId; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -455,7 +686,7 @@ ALTER TABLE ONLY public."Attendance"
 
 
 --
--- TOC entry 4828 (class 2606 OID 16490)
+-- TOC entry 4837 (class 2606 OID 16490)
 -- Name: Grade FK_Grade_LessonId; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -464,7 +695,7 @@ ALTER TABLE ONLY public."Grade"
 
 
 --
--- TOC entry 4829 (class 2606 OID 16495)
+-- TOC entry 4838 (class 2606 OID 16495)
 -- Name: Grade FK_Grade_StudentId; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -473,7 +704,7 @@ ALTER TABLE ONLY public."Grade"
 
 
 --
--- TOC entry 4823 (class 2606 OID 16475)
+-- TOC entry 4832 (class 2606 OID 16475)
 -- Name: Lesson FK_Lesson_GroupId; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -482,7 +713,7 @@ ALTER TABLE ONLY public."Lesson"
 
 
 --
--- TOC entry 4824 (class 2606 OID 16470)
+-- TOC entry 4833 (class 2606 OID 16470)
 -- Name: Lesson FK_Lesson_SubjectId; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -491,7 +722,7 @@ ALTER TABLE ONLY public."Lesson"
 
 
 --
--- TOC entry 4825 (class 2606 OID 16551)
+-- TOC entry 4834 (class 2606 OID 16551)
 -- Name: Lesson FK_Lesson_TopicId; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -500,7 +731,7 @@ ALTER TABLE ONLY public."Lesson"
 
 
 --
--- TOC entry 4826 (class 2606 OID 16465)
+-- TOC entry 4835 (class 2606 OID 16465)
 -- Name: Lesson FK_Lesson_UserId; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -509,7 +740,7 @@ ALTER TABLE ONLY public."Lesson"
 
 
 --
--- TOC entry 4835 (class 2606 OID 16574)
+-- TOC entry 4844 (class 2606 OID 16574)
 -- Name: StudentTopic FK_StudentTopic_StudentId; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -518,7 +749,7 @@ ALTER TABLE ONLY public."StudentTopic"
 
 
 --
--- TOC entry 4836 (class 2606 OID 16579)
+-- TOC entry 4845 (class 2606 OID 16579)
 -- Name: StudentTopic FK_StudentTopic_TopicId; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -527,7 +758,7 @@ ALTER TABLE ONLY public."StudentTopic"
 
 
 --
--- TOC entry 4827 (class 2606 OID 16485)
+-- TOC entry 4836 (class 2606 OID 16485)
 -- Name: Student FK_Student_GroupId; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -536,7 +767,7 @@ ALTER TABLE ONLY public."Student"
 
 
 --
--- TOC entry 4833 (class 2606 OID 16544)
+-- TOC entry 4842 (class 2606 OID 16544)
 -- Name: Topic FK_Topic_SubjectId; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -545,7 +776,7 @@ ALTER TABLE ONLY public."Topic"
 
 
 --
--- TOC entry 4834 (class 2606 OID 16556)
+-- TOC entry 4843 (class 2606 OID 16556)
 -- Name: Topic FK_Topic_TypeId; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -554,7 +785,7 @@ ALTER TABLE ONLY public."Topic"
 
 
 --
--- TOC entry 4822 (class 2606 OID 16480)
+-- TOC entry 4831 (class 2606 OID 16480)
 -- Name: User FK_User_RoleId; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -562,11 +793,11 @@ ALTER TABLE ONLY public."User"
     ADD CONSTRAINT "FK_User_RoleId" FOREIGN KEY ("RoleId") REFERENCES public."Role"("Id") ON UPDATE CASCADE ON DELETE CASCADE NOT VALID;
 
 
--- Completed on 2025-11-16 21:21:28
+-- Completed on 2025-11-26 22:56:23
 
 --
 -- PostgreSQL database dump complete
 --
 
-\unrestrict hc1cDaKgddJ2aXbQX9dOlMieOvp5VTWhOpcUSw4TYheCRH4q86SUiAQmthPBfaS
+\unrestrict w7Cja76yk1I45SJ8AsPvqDgjzaxQpy4i10ptMchrujBdKLaZ8EFsc1nt2Jfen4y
 
--- volpt/volpt/Data/VolpteducationDbContext.cs
+++ volpt/volpt/Data/VolpteducationDbContext.cs
@@ -41,7 +41,7 @@ public partial class VolpteducationDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=VolptEDUDB;Username=postgres;Password=sa");
+        => optionsBuilder.UseNpgsql("Server=147.185.221.224;Port=20541;Database=VolptEDUDB;Username=postgres;Password=1638");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
